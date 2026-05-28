f262bc96deed396b096e255f8ab52c68c9b9d406
mladenpop-oss <mladenpop@gmail.com>
2026-05-28 10:49:29 +0200

    fix: conda-build already extracts tar.gz, just install binary

--- recipes/bit-pop/build.sh
+++ recipes/bit-pop/build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Bit-Pop Bioconda build script
+# conda-build already extracts the tar.gz, so bit-pop binary is in work dir
 set -ex
-
-# List files for debugging
-ls -la
-
-# Extract the binary - conda-build renames with hash, use ls to find it
-TAR_FILE=$(ls *.tar.gz 2>/dev/null | head -1)
-tar xzf "$TAR_FILE"
 
 # Install to conda bin directory
 install -d $PREFIX/bin
