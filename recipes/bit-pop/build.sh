e76d782d32b16c29dd9c695e49217a3a4cd67001
mladenpop-oss <mladenpop@gmail.com>
2026-05-28 11:56:03 +0200

    refactor: build from source with cargo instead of pre-built binary (GLIBC compatibility)

--- recipes/bit-pop/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Bit-Pop Bioconda build script
-# conda-build already extracts the tar.gz, so bit-pop binary is in work dir
-set -ex
-
-# Install to conda bin directory
-install -d $PREFIX/bin
-install -m 755 bit-pop $PREFIX/bin/bit-pop
